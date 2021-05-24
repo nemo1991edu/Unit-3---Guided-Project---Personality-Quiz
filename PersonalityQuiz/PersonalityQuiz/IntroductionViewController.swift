0bc5cb46c0e026350ebdf77f8f50951d7bd70798
nemo1991edu <70537469+nemo1991edu@users.noreply.github.com>
2021-05-24 02:09:52 -0700

    Add files via upload

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,18 @@
+
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func unwindToQuizIntroduction(segue:
+    UIStoryboardSegue) {
+    }
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,51 @@
+
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+//    let frequentAnswersSorted = frequencyOfAnswers.sorted(by:
+//    { (pair1, pair2) in return pair1.value > pair2.value
+//})
+    
+//    let mostCommonAnswer = frequencyOfAnswers.sorted { $0.1 >
+//       $1.1 }.first!.key
+    
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    
+    var responses: [Answer]
+    
+    init?(coder: NSCoder, responses: [Answer]) {
+        self.responses = responses
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
+
+    }
+    func calculatePersonalityResult() {
+     let frequencyOfAnswers = responses.reduce(into:[AnimalType: Int]()) { (counts, answer) in
+        if let existingCount = counts[answer.type] {
+            counts[answer.type] = existingCount + 1
+        } else {
+            counts[answer.type] = 1
+        }
+        }
+//        let frequentAnswersSorted = frequencyOfAnswers.sorted(by:
+//        { (pair1, pair2) in
+//            return pair1.value > pair2.value
+            let mostCommonAnswer = frequencyOfAnswers.sorted { $0.1 >
+                $1.1 }.first!.key
+            resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+    resultDefinitionLabel.text = mostCommonAnswer.definition
+        }
+//    )
+    }
+//}
